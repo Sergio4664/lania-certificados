8d31a94351a05e859126d61f1e2de040ea649edd
Sergio4664 <sergiocervantes742@gmail.com>
2025-11-23 20:11:38 -0600

    Primer despliege 2

--- frontend/lania-ui/src/environments/environment.prod.ts
+++ frontend/lania-ui/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
-// src/environments/environment.prod.ts
-export const environmentProd = {
-  production: true,
-  apiUrl: 'http://127.0.0.1:8000',
-  apiBase: 'http://127.0.0.1:8000'  // Added apiBase property
+export const environment = {
+  // Siempre true para producción
+  production: true, 
+  
+  // 🚨 Usar el dominio final con el protocolo HTTPS (dado el puerto 8443)
+  // Añadimos el prefijo /api/v1 (estándar de FastAPI)
+  apiUrl: 'https://siscol.lania.mx:8443/api/v1', 
+
+  // URL base para la verificación pública y QR codes (opcional si es necesario)
+  baseUrl: 'https://siscol.lania.mx:8443/' 
 };
